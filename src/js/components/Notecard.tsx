a3fe7b3030c632e71be8bc73d6b257a1744b9666
siya-vashi <siyav@umich.edu>
2026-02-03 21:24:36 -0500

    added random rotations (polish later)

--- src/js/components/Notecard.tsx
+++ src/js/components/Notecard.tsx
@@ -1,5 +1,5 @@
 import '../../css/Notecard.css';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 interface NotecardProps {
 	text: string;
@@ -10,13 +10,23 @@ const Notecard: React.FC<NotecardProps> = ({ text }) => {
 
 	const toggle = () => setFlipped((f) => !f);
 
+	const rotationA = useRef(`${Math.random() * 10 - 5}deg`); // 0 to 5
+	const rotationB = useRef(`${Math.random() * 10 - 5}deg`); 
+	
+	
+	
 	// this could even have a function for the direction cards face
 	return (
+		
 		<div
 			className={`wrapper ${!flipped ? 'back flipped' : 'front'}`}
 			onMouseOver={toggle}
 			onMouseLeave={toggle}
 			aria-pressed={flipped}
+			style={{
+				'--rota': rotationA.current, 
+				'--rotb': rotationB.current
+			  } as React.CSSProperties}
 		>
 			{/* <div className={`lid one ${color}`}></div>
 			<div className={`lid two ${color}`}></div>
