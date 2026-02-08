912e7827c77314412791490ae36730a40f1511d2
Daniel Liu <dlliu@umich.edu>
2026-02-08 15:19:58 -0500

    add progress 4 and remove disable

--- src/js/components/ArticleGallery.tsx
+++ src/js/components/ArticleGallery.tsx
@@ -6,12 +6,13 @@ import ProgressBar0 from '../../assets/progress-bar-0.svg';
 import ProgressBar1 from '../../assets/progress-bar-1.svg';
 import ProgressBar2 from '../../assets/progress-bar-2.svg';
 import ProgressBar3 from '../../assets/progress-bar-3.svg';
+import ProgressBar4 from '../../assets/progress-bar-4.svg';
 import Article from './Article';
 
 const ArticleGallery = ({ articles, index, setIndex }: ArticleListProps) => {
 	// articles is a list of JSON
 
-	const progressBars = [ProgressBar0, ProgressBar1, ProgressBar2, ProgressBar3];
+	const progressBars = [ProgressBar0, ProgressBar1, ProgressBar2, ProgressBar3, ProgressBar4];
 
 	return (
 		<>
@@ -19,24 +20,24 @@ const ArticleGallery = ({ articles, index, setIndex }: ArticleListProps) => {
 				<Article {...articles[index]} />
 				<div className="arrows-and-progress-bar">
 					{/* left arrow button */}
-					{index > 0 ? (
-						<button onClick={() => setIndex(index - 1)} className="arrow left">
-							<img src={LeftArrow} alt='Right arrow button' />
-						</button>
-					) : (
-						<button className="arrow left placeholder" disabled></button>
-					)}
+					<button 
+						onClick={() => setIndex(index - 1)} 
+						className="arrow left"
+						disabled={index === 0}
+					>
+						<img src={LeftArrow} alt='Left arrow button' />
+					</button>
 
 					<img src={progressBars[index]} alt='Heart progress bar' />
 
-					{/* Right arrow button (only shows if index < text.length - 1) */}
-					{index < articles.length - 1 ? (
-						<button onClick={() => setIndex(index + 1)} className="arrow right">
-							<img src={RightArrow} alt='Right arrow button' />
-						</button>
-					) : (
-						<button className="arrow right placeholder" disabled></button>
-					)}
+					{/* right arrow button */}
+					<button 
+						onClick={() => setIndex(index + 1)} 
+						className="arrow right"
+						disabled={index === articles.length - 1}
+					>
+						<img src={RightArrow} alt='Right arrow button' />
+					</button>
 				</div>
 			</div>
 		</>
