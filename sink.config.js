bfa0fc0880f03ac80622b3c395778a8d56add0f2
meijja <juliamei@umich.edu>
2026-02-11 00:34:17 -0500

    Love notes now show current messages

--- sink.config.js
+++ sink.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
 			type: 'sheet',
 			id: '1cySYT_FHwaJsiKfyX1pHcK9vLOL8u3TvzbpEVifRxhc',
 			sheetId: '1850985461',
-			output: 'src/data/data.json',
+			output: 'src/data.json',
 			auth: '~/.daily-google-services.json'
 		},
 		{
--- src/js/components/Grid.tsx
+++ src/js/components/Grid.tsx
@@ -2,45 +2,51 @@ import Notecard from './Notecard';
 import Button from '../../assets/button.svg';
 import '../../css/Grid.css';
 import messages from '../../data.json';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
-interface Messages {
-	[key: string]: string;
+interface Message {
+	Timestamp: string;
+	'Your email': string;
+	'Sender Name (If you want to be anonymous, leave this blank!)': string;
+	'Receiver Name': string;
+	'Receiver Email': string;
+	Message: string;
 }
 
 const Grid: React.FC = () => {
 	// const messages = ['Hey', 'Wassup', 'Hello', 'Happy birthday Jeff! Have a good birthday! This is a really long example message because i want to check if scrolling works']; // Example messages
 
-	const colors = ['blue', 'pink', 'yellow']; // Choose colors you want idk
-
 	// Explicitly assert that 'messages' is of type Messages
-	const typedMessages = messages as Messages;
-	const keyArr = Object.keys(typedMessages);
+	const typedMessages: Message[] = messages;
 
 	// Shuffle messages using the Fisher-Yates algorithm
-	for (let i = keyArr.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1));
-		[keyArr[i], keyArr[j]] = [keyArr[j], keyArr[i]];
-	}
+	const shuffledMessages = useMemo(() => {
+		const copy = [...typedMessages];
+
+		for (let i = copy.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[copy[i], copy[j]] = [copy[j], copy[i]];
+		}
+
+		return copy;
+	}, [typedMessages]);
 
 	const [visibleNotes, setVisibleNotes] = useState(10);
 
 	// Function to render only the visible notecards
 	const renderNotecards = () => {
-		return keyArr.slice(0, visibleNotes).map((key, index) => {
-			const message = typedMessages[key];
-			const color = colors[index % colors.length]; // Alternate colors
+		return shuffledMessages.slice(0, visibleNotes).map((message, index) => {
 
 			return (
-				<div key={key} className="grid-item">
-					<Notecard text={message} color={color} />
+				<div key={index} className="grid-item">
+					<Notecard text={message.Message}/>
 				</div>
 			);
 		});
 	};
 
 	const loadMoreNotes = () => {
-		setVisibleNotes((prev) => Math.min(prev + 10, keyArr.length));
+		setVisibleNotes((prev) => Math.min(prev + 10, shuffledMessages.length));
 	};
 
 	return (
